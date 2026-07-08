d158377d6f126147d21a1aee67a80f034fed31cf
Cat <catherinenishinoyayuu@gmail.com>
2026-07-07 21:10:43 -0400

    make everyone member

--- src/Sanctuary.Login/Handlers/CharacterSelectInfoRequestHandler.cs
+++ src/Sanctuary.Login/Handlers/CharacterSelectInfoRequestHandler.cs
@@ -54,7 +54,7 @@ public static class CharacterSelectInfoRequestHandler
             .AsSplitQuery()
             .ToList();
 
-        characterSelectInfoReply.Status = characters.Count == 0 ? 2 : 1;
+        characterSelectInfoReply.Status = 1;
 
         foreach (var character in characters)
         {
