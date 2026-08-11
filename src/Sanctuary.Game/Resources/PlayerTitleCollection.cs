52897181e4fbfc7716c583fef7a1cbc6f866eccb
unknown <catherinenishinoyayuu@gmail.com>
2026-08-11 13:48:46 -0400

    test

--- src/Sanctuary.Game/Resources/PlayerTitleCollection.cs
+++ src/Sanctuary.Game/Resources/PlayerTitleCollection.cs
@@ -43,7 +43,7 @@ public class PlayerTitleCollection : ObservableConcurrentDictionary<int, PlayerT
                 _logger.LogError("No entries found in file \"{file}\".", filePath);
                 return false;
             }
-
+            Clear();
             foreach (var entry in list)
             {
                 if (!TryAdd(entry.Id, entry))
