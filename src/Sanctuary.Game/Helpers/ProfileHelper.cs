1b0284081cd67383a9c4239030893e5a339264de
unknown <catherinenishinoyayuu@gmail.com>
2026-08-29 14:01:25 -0400

    changed single or default to firstordefault

--- src/Sanctuary.Game/Helpers/ProfileHelper.cs
+++ src/Sanctuary.Game/Helpers/ProfileHelper.cs
@@ -7,10 +7,6 @@ namespace Sanctuary.Game.Helpers;
 
 public static class ProfileHelper
 {
-    /// <summary>
-    /// Grants a character the items listed in a profile's <see cref="ClientProfileData.DefaultItems"/>,
-    /// reusing any matching item the character already owns, and attaches them to the given profile.
-    /// </summary>
     public static void GrantDefaultItems(DbCharacter character, DbProfile dbProfile,
         ClientProfileData profileData, IResourceManager resourceManager)
     {
@@ -22,7 +18,7 @@ public static class ProfileHelper
             if (defaultClientItemDefinition.GenderUsage != 0 && defaultClientItemDefinition.GenderUsage != character.Gender)
                 continue;
 
-            var dbItem = character.Items.SingleOrDefault(x => x.Definition == defaultItemId);
+            var dbItem = character.Items.FirstOrDefault(x => x.Definition == defaultItemId);
 
             if (dbItem is null)
             {
